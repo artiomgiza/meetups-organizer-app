f12801e5398ea76e8abdc5c64086de90c2cac2c9
Artiom Giza <artiomg@gettaxi.com>
2018-07-28 01:06:43 +0300

    add meet up creation locally - finish 10

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,106 @@
 <template>
-  <div>
-    <p>The create meetup page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h2 class="secondary--text">Create a new meetup</h2>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form @submit.prevent="onCreateMeetup">
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="title"
+              label="Title"
+              id="title"
+              v-model="title"
+              required></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="location"
+              label="Location"
+              id="location"
+              v-model="location"
+              append-icon="place"
+              required></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="imageUrl"
+              label="Image URL"
+              id="image-url"
+              v-model="imageUrl"
+              append-icon="image"
+              required></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" width="100%">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-textarea
+              name="description"
+              label="Description"
+              id="description"
+              v-model="description"
+              multi-line
+              required></v-textarea>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary"
+              :disabled="!formIsValid"
+              type="submit"
+               >Create Meetup</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      location: '',
+      description: '',
+      imageUrl: ''
+    }
+  },
+  computed: {
+    formIsValid () {
+      return this.title !== '' && this.location !== ''
+    }
+  },
+  methods: {
+    onCreateMeetup () {
+      if (!this.formIsValid) {
+        return
+      }
+      const meetupData = {
+        title: this.title,
+        location: this.location,
+        imageUrl: this.imageUrl,
+        description: this.description,
+        date: new Date()
+      }
+      console.log('asfd')
+      this.$store.dispatch('createMeetup', meetupData)
+      this.$router.push('/meetups')
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -24,8 +24,26 @@ export const store = new Vuex.Store({
       registeredMeetups: ['1231']
     }
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    }
+  },
+  actions: {
+    createMeetup ({commit}, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: 'asdasdafs'
+      }
+      // Reach out to FB and store it
+      // this will return us ID and we'll store it...
+      commit('createMeetup', meetup)
+    }
+  },
   getters: {
     loadedMeetups (state) {
       return state.loadedMeetups.sort((meetupA, meetupB) => {

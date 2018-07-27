0758c15825fbcaef4c417d4a2fea96ae460fd991
Artiom Giza <artiomg@gettaxi.com>
2018-07-27 12:16:28 +0300

    add navigation buttons. different classes take power in different sizes!

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <p>The home page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="primary">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="primary">Orginize Meetups</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>

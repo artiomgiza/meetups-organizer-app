4d00dc697ecfaf6eee6fc183e03917fa193453e6
Artiom Giza <artiomg@gettaxi.com>
2018-08-06 15:27:12 +0300

    add simple map + markers

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -1,8 +1,47 @@
  <template>
   <v-container>
-
-  <br>
   <v-layout row>
+
+    <v-flex xs12 sm6 offset-sm3>
+
+      <GmapAutocomplete @place_changed="setPlace"></GmapAutocomplete>
+      <v-btn @click="usePlace">Add</v-btn>
+
+      <br/><br/>
+
+      <GmapMap
+        ref="mapRef"
+        :center="center"
+        :zoom="15"
+        map-type-id="terrain"
+        style="width: 100%; height: 350px"
+      >
+        <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center=m.position"
+        />
+        <GmapMarker
+              v-if="this.place"
+              label="★"
+              :position="{
+                lat: this.place.geometry.location.lat(),
+                lng: this.place.geometry.location.lng(),
+              }"
+              />
+
+      </GmapMap>
+
+      <v-divider class="mt-4 mb-4"></v-divider>
+    </v-flex>
+
+  </v-layout>
+
+  <v-layout row>
+
     <v-flex xs12 sm6 offset-sm3>
       <v-avatar
         color="primary"
@@ -44,10 +83,44 @@
 export default {
   data () {
     return {
-
+      center: {lat: 32.073480, lng: 34.783966},
+      place: null,
+      markers: [
+        {position: {lat: 32.073480, lng: 34.783966}},
+        {position: {lat: 32.075480, lng: 34.782166}}
+      ]
     }
   },
+  mounted () {
+    // At this point, the child GmapMap has been mounted, but
+    // its map has not been initialized.
+    // Therefore we need to write mapRef.$mapPromise.then(() => ...)
+
+    this.$refs.mapRef.$mapPromise.then((map) => {
+      this.getLocation()
+      map.panTo(this.center)
+    })
+  },
   methods: {
+    setPlace (place) {
+      this.place = place
+      this.center = {
+        lat: this.place.geometry.location.lat(),
+        lng: this.place.geometry.location.lng()
+      }
+    },
+    usePlace (place) {
+      if (this.place) {
+        this.markers.push({
+          position: {
+            lat: this.place.geometry.location.lat(),
+            lng: this.place.geometry.location.lng()
+          }
+        })
+        this.place = null
+      }
+    },
+
     openCenter () {
       this.$toast(':) :) :) :)')
     },
@@ -60,8 +133,17 @@ export default {
     },
     closeLoading () {
       this.$loading.close()
+    },
+    getLocation () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.center = {lat: position.coords.latitude, lng: position.coords.longitude}
+          this.markers.push({position: this.center})
+        })
+      } else {
+        this.center = {lat: 32.073480, lng: 34.783966}
+      }
     }
-
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 
 import * as firebase from 'firebase'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 import 'vue2-toast/lib/toast.css'
 import Toast from 'vue2-toast'
@@ -26,6 +27,37 @@ Vue.use(Toast, {
   wordWrap: true,
   width: '280px'
 })
+
+/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// GOOGLE MAPS SECTION - START /////////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAbsB27Z4nOxdxzWA-otRmUTP-y40xMgsc',
+    libraries: 'places' // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    /// / If you want to set the version, you can do so:
+    // v: '3.26',
+  }
+
+  /// / If you intend to programmatically custom event listener code
+  /// / (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  /// / instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  /// / you might need to turn this on.
+  // autobindAllEvents: false,
+
+  /// / If you want to manually install components, e.g.
+  /// / import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+  /// / Vue.component('GmapMarker', GmapMarker)
+  /// / then disable the following:
+  // installComponents: true,
+})
+/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// GOOGLE MAPS SECTION - FINISH ////////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false

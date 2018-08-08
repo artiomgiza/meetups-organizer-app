aebf0d10d385b0157d4917bbb87622b96c418931
Artiom Giza <artiomg@gettaxi.com>
2018-08-08 16:34:55 +0300

    update edit component with parallax

--- src/components/Meetup/Edit/EditMeetup.vue
+++ src/components/Meetup/Edit/EditMeetup.vue
@@ -1,101 +1,115 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
-        <h2 class="secondary--text">Edit a new meetup</h2>
-      </v-flex>
-    </v-layout>
+    <form @submit.prevent="onUpdateMeetup">
 
-    <v-layout row>
-      <v-flex xs12>
-        <form @submit.prevent="onUpdateMeetup">
+      <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+          <v-parallax
+            height="40"
+            :src="imageUrl"
+            style="opacity: .3;"
+            class="mb-2">
 
-          <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
-              <v-text-field
-              name="title"
-              label="Title"
-              id="title"
-              v-model="title"
-              required></v-text-field>
+            <v-layout
+              align-center
+              justify-center
+              column
+            >
+              <v-flex class="black--text">
+                <h1>Edit</h1>
+              </v-flex>
+            </v-layout>
+          </v-parallax>
+        </v-flex>
+      </v-layout>
+
+
+      <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+          <v-text-field
+          name="title"
+          label="Title"
+          id="title"
+          v-model="title"
+          required></v-text-field>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+          <v-textarea
+          name="description"
+          label="Description"
+          id="description"
+          v-model="description"
+          multi-line
+          rows="3"
+          required>
+          </v-textarea>
+        </v-flex>
+      </v-layout>
+
+
+      <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+          <app-editable-map
+          v-on:meetup-location="setLocation($event)"
+          :locationLatLng="locationLatLng">
+          </app-editable-map>
+          </v-flex>
+      </v-layout>
+
+      <v-layout  row>
+        <v-flex xs12 sm6 offset-sm3 class="mb-2" >
+
+            <v-divider class="mb-3"></v-divider>
+
+            <app-date-time-dialog class="mb-3"
+            v-on:meetup-date-time="dateTime = $event">
+            </app-date-time-dialog>
+
+            <v-divider class="mb-3"></v-divider>
+
+        </v-flex>
+      </v-layout>
+
+      <v-layout  row>
+        <v-flex xs12 sm6 offset-sm3 class="mb-2" >
+
+          <v-parallax
+          height="280"
+          :src="imageUrl"
+          style=" opacity: .3;"
+          >
+            <v-flex class="black--text">
+              <h1>Meetup Image</h1>
             </v-flex>
-          </v-layout>
+          </v-parallax>
+        </v-flex>
+      </v-layout>
 
-          <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
-              <v-textarea
-              name="description"
-              label="Description"
-              id="description"
-              v-model="description"
-              multi-line
-              required>
-              </v-textarea>
-            </v-flex>
-          </v-layout>
+      <v-layout row >
+        <v-flex xs12 sm6 offset-sm3 class="text-xs-center">
 
-          <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
-              <app-editable-map
-              v-on:meetup-location="setLocation($event)"
-              :locationLatLng="locationLatLng">
-              </app-editable-map>
+          <v-btn
+          class="success"
+          large
+          :disabled="!formIsValid"
+          type="submit">
+            <v-icon left>check_circle</v-icon>
+            Update Meetup
+            </v-btn>
 
-              <v-divider class="mt-4 mb-4"></v-divider>
-             </v-flex>
-          </v-layout>
+            <v-btn
+            flat
+            class="blue--text darken-1"
+            :to="'/meetups/'+ id">
+            Cancel
+          </v-btn>
 
-           <v-layout row>
-            <v-flex xs8 sm6 offset-sm3 class="mb-2" >
-                <app-date-time-dialog
-                v-on:meetup-date-time="dateTime = $event">
-                </app-date-time-dialog>
-            </v-flex>
-          </v-layout>
-
-          <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
-              <v-btn raised class="primary" @click="onPickFile" disabled>
-                <v-icon dark left>add_photo_alternate</v-icon>
-                Change image - not spported</v-btn>
-              <input
-              type="file"
-              style="display: none"
-              ref="fileInput"
-              accept="image/*"
-              @change="onFilePicked">
-            </v-flex>
-          </v-layout>
-
-          <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
-              <img :src="imageUrl" width="50%" class="ml-2 mt-3 mb-2">
-            </v-flex>
-          </v-layout>
-
-          <v-layout row >
-            <v-flex xs12 sm6 offset-sm3 class="text-xs-center">
-              <v-btn
-                flat
-                class="blue--text darken-1"
-                :to="'/meetups/'+ id">
-                Cancel
-              </v-btn>
-
-              <v-btn
-              class="success"
-              large
-              :disabled="!formIsValid"
-              type="submit">
-               <v-icon left>check_circle</v-icon>
-               Update Meetup
-               </v-btn>
-            </v-flex>
-          </v-layout>
-
-        </form>
-      </v-flex>
-    </v-layout>
+        </v-flex>
+      </v-layout>
+    </form>
   </v-container>
 </template>
 
